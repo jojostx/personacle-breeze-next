9b65b1303b82dc11435ea40e80a2417e1d21cec6
Ari Jay <ikuskid7@gmail.com>
2023-06-19 19:36:48 +0100

    wip

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,11 +5,12 @@ export default function Banner({
     position = 'bottom',
     color = 'white',
     isOpen = true,
+    showCloseButton = true,
 }) {
     const [bannerOpen, setBannerOpen] = useState(isOpen)
 
     const positions = {
-        top: 'top-0 inset-x-0',
+        top: 'top-0 inset-x-0 shadow',
         topLeft: 'top-0 left-0 md:left-12 md:top-8',
         topRight: 'top-0 right-0 md:right-12 md:top-8',
         bottom: 'bottom-0 inset-x-0',
@@ -62,29 +63,40 @@ export default function Banner({
         },
     }
 
-    const _position =
-        position in positions ? positions[position] : positions['bottom']
+    const shadows = {
+        top: 'shadow-lg',
+        topLeft: 'shadow-lg',
+        topRight: 'shadow-lg',
+        bottom: 'shadow-t-lg',
+        bottomLeft: 'shadow-t-lg',
+        bottomRight: 'shadow-t-lg',
+    }
+
+    const _position = position in positions ? positions[position] : positions['bottom']
     const _color = color in colors ? colors[color] : colors['white']
+    const shadow = position in shadows ? shadows[position] : shadows['top']
 
     return (
         <>
             {bannerOpen && (
                 <div className={`${_position} fixed w-full md:w-auto z-50`}>
                     <div
-                        className={`flex items-center justify-between p-2 text-sm shadow-lg ${_color.background} md:rounded`}>
+                        className={`flex items-center justify-between p-2 px-4 text-sm ${shadow} ${_color.background} md:rounded`}>
                         <div className={`flex-1 ${_color.text}`}>
                             {children}
                         </div>
-                        <button
-                            className={`p-3 ml-3 border-l ${_color.border} ${_color.buttonIcon} ${_color.buttonIconHover}`}
-                            onClick={() => setBannerOpen(false)}>
-                            <span className="sr-only">Close</span>
-                            <svg
-                                className="w-4 h-4 fill-current shrink-0"
-                                viewBox="0 0 16 16">
-                                <path d="M12.72 3.293a1 1 0 00-1.415 0L8.012 6.586 4.72 3.293a1 1 0 00-1.414 1.414L6.598 8l-3.293 3.293a1 1 0 101.414 1.414l3.293-3.293 3.293 3.293a1 1 0 001.414-1.414L9.426 8l3.293-3.293a1 1 0 000-1.414z" />
-                            </svg>
-                        </button>
+                        {showCloseButton && (
+                            <button
+                                className={`p-3 ml-3 border-l ${_color.border} ${_color.buttonIcon} ${_color.buttonIconHover}`}
+                                onClick={() => setBannerOpen(false)}>
+                                <span className="sr-only">Close</span>
+                                <svg
+                                    className="w-4 h-4 fill-current shrink-0"
+                                    viewBox="0 0 16 16">
+                                    <path d="M12.72 3.293a1 1 0 00-1.415 0L8.012 6.586 4.72 3.293a1 1 0 00-1.414 1.414L6.598 8l-3.293 3.293a1 1 0 101.414 1.414l3.293-3.293 3.293 3.293a1 1 0 001.414-1.414L9.426 8l3.293-3.293a1 1 0 000-1.414z" />
+                                </svg>
+                            </button>
+                        )}
                     </div>
                 </div>
             )}
--- src/pages/questions.js
+++ src/pages/questions.js
@@ -6,12 +6,13 @@ import Question from '@/components/Question'
 import { useStateContext } from '@/contexts/QuestionContext'
 import useWatch from '@/hooks/useWatch'
 import axios from '@/lib/axios'
-import { Progress, Toast } from 'flowbite-react'
+import { Progress } from 'flowbite-react'
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
 
 const Questions = () => {
     const [questions, setQuestions] = useState()
+    const [showProgressBar, setShowProgressBar] = useState(false)
     const [hasFormError, setHasFormError] = useState(false)
     const [formError, setFormError] = useState('')
     const {
@@ -35,6 +36,10 @@ const Questions = () => {
     }, [])
 
     useWatch(() => {
+        if (questionsAnswered.answers.size > 0) {
+            setShowProgressBar(true)
+        }
+
         if (questionsAnswered.answers.size == totalQuestions) {
             hideError()
         }
@@ -76,10 +81,11 @@ const Questions = () => {
     }
 
     const getPercentageAnswered = () => {
-        console.log(questionsAnswered.answers.size, totalQuestions)
-        return Math.round(
-            (questionsAnswered.answers.size / totalQuestions) * 100,
-        )
+        let percentage = questionsAnswered.answers.size / totalQuestions
+
+        if (!percentage) percentage = 0
+
+        return Math.round(percentage * 100)
     }
 
     return (
@@ -89,25 +95,26 @@ const Questions = () => {
                     Questions
                 </h2>
             }>
-            
             <Head>
                 <title>Personacle - Questions</title>
             </Head>
 
-            <Banner isOpen={true} position={'top'}>
-                <Progress
-                    labelProgress
-                    labelText
-                    color="indigo"
-                    progress={getPercentageAnswered()}
-                    size="md"
-                    textLabel="You have answered:"
-                    progressLabelPosition="outside"
-                    textLabelPosition="outside"
-                />
-            </Banner>
-            
-            <div className="pt-4">
+            {showProgressBar && (
+                <Banner isOpen={true} showCloseButton={false} position="bottom">
+                    <Progress
+                        labelProgress
+                        labelText
+                        color="indigo"
+                        progress={getPercentageAnswered()}
+                        size="md"
+                        textLabel="You have answered:"
+                        progressLabelPosition="outside"
+                        textLabelPosition="outside"
+                    />
+                </Banner>
+            )}
+
+            <div className="pt-4 pb-24">
                 <form
                     onSubmit={submitForm}
                     className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
@@ -124,7 +131,7 @@ const Questions = () => {
                             </div>
                         )}
                     </div>
-                    <div className="flex items-center justify-center w-full my-4">
+                    <div className="flex items-center justify-center w-full mt-4">
                         <Button className="items-center justify-center">
                             Submit
                             <ArrowIcon className="w-4 h-4" />
