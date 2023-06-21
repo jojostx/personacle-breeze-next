c2b878e62ad858444e56c3101ba5f42b9b791811
Ari Jay <ikuskid7@gmail.com>
2023-06-21 11:50:48 +0100

    add route to result page from dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -62,8 +62,8 @@ const Dashboard = () => {
                                 with easily understandable visualizations.
                             </p>
 
-                            <a
-                                href="#"
+                            <Link
+                                href="/questions"
                                 className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg justify-self-end max-w-fit hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 Share
                                 <svg
@@ -77,7 +77,7 @@ const Dashboard = () => {
                                         d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
                                         clipRule="evenodd"></path>
                                 </svg>
-                            </a>
+                            </Link>
                         </div>
                     </div>
                 </div>
